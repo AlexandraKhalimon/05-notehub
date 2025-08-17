0b0ed9fe161cb057f2ae345a68179425ba3da397
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-08-17 18:02:30 +0300

    fix errors

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,9 +6,9 @@ import Pagination from '../Pagination/Pagination';
 import Modal from '../Modal/Modal';
 import NoteForm from '../NoteForm/NoteForm';
 import SearchBox from '../SearchBox/SearchBox';
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
+import { useDebounce } from 'use-debounce';
 import toast, { Toaster } from 'react-hot-toast';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
@@ -19,10 +19,12 @@ export default function App() {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 300);
 
   const { data, isSuccess, isLoading, isError } = useQuery({
-    queryKey: ['notes', currentPage, searchQuery],
-    queryFn: () => fetchNotes({search: searchQuery, page: currentPage, perPage: 12}),
+    queryKey: ['notes', currentPage, debouncedSearchQuery],
+    queryFn: () => fetchNotes({ search: debouncedSearchQuery, page: currentPage, perPage: 12 }),
+    placeholderData: keepPreviousData
   })
 
   useEffect(() => {
@@ -42,20 +44,23 @@ export default function App() {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  const deboucedSearchQuery = useDebouncedCallback(setSearchQuery, 300);
+  const handleChange = (query: string) => {
+    setSearchQuery(query);
+    setCurrentPage(1);
+  };
   
   return (
     <div className={css.app}>
       <Toaster position='top-center'/>
       <header className={css.toolbar}>
-        <SearchBox text={ searchQuery } onSearch={deboucedSearchQuery}/>
+        <SearchBox text={ searchQuery } onSearch={handleChange}/>
         {totalPages > 1 && <Pagination totalPages={totalPages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>}
         <button className={css.button} onClick={openModal}>Create note +</button>
         {isModalOpen && <Modal onClose={closeModal} children={<NoteForm onClose={closeModal} onSuccess={closeModal}/>} />}
       </header>
       {isLoading && <Loader />}
       {isError && <ErrorMessage/>}
-      {data?.notes.length !== 0 && <NoteList notes={notes}/>}
+      {data && data?.notes.length > 0 && <NoteList notes={notes}/>}
     </div>
   )
 };
--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,21 +1,43 @@
+// import css from './SearchBox.module.css';
+
+// interface SearchBoxProps {
+//     text: string;
+//     onSearch: (newSearchQuery: string) => void;
+// }
+
+// export default function SearchBox({text, onSearch}: SearchBoxProps) {
+//     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         onSearch(event.target.value);
+//     }
+    
+//     return (
+//         <input
+//             className={css.input}
+//             type="text"
+//             defaultValue={text}
+//             onChange={handleChange}
+//             placeholder="Search notes"
+//         />
+//     );
+// }
+
+
 import css from './SearchBox.module.css';
 
 interface SearchBoxProps {
     text: string;
-    onSearch: (newSearchQuery: string) => void;
+    onSearch: (query: string) => void;
 }
 
 export default function SearchBox({text, onSearch}: SearchBoxProps) {
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onSearch(event.target.value);
-    }
+    
     
     return (
         <input
             className={css.input}
             type="text"
             defaultValue={text}
-            onChange={handleChange}
+            onChange={(event)=> onSearch(event.target.value)}
             placeholder="Search notes"
         />
     );
